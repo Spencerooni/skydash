97235e5a7837c95964378c689ed4db5bfc846e41
David Spence <d.spence@kainos.com>
2016-04-29 20:37:52 +0100

    Initial commit with simple response classes and basic test data json.

---
+++ Skydash/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Skydash.Response;
+
+namespace Skydash
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string json;
+
+            using (StreamReader r = new StreamReader("data.json"))
+            {
+                json = r.ReadToEnd();
+            }
+
+            var result = JsonConvert.DeserializeObject<Dictionary<string, VAppAndMachineWrapper>>(json);
+        }
+    }
+}
